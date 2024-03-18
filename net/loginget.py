9da8e08f23d58206f9619981e44c011f17fafaf2
R <ivanovich343@gmail.com>
2024-03-18 13:43:24 +0200

    get login

--- net/loginget.py
+++ net/loginget.py
@@ -1,13 +1,25 @@
 import os
+import pwd
 
 print("start!")
 osLoginGet = os.getlogin()
 
-print("os: ", osLoginGet)
+print("os get login ->", osLoginGet)
 
 nameLogin = osLoginGet
 inputLoginStr = 'Input login ['+nameLogin+'] '
 inputLoginGet = input(inputLoginStr)
 if inputLoginGet != "": nameLogin = inputLoginGet
-print("login: ",nameLogin)
+print("login -> ",nameLogin)
 
+print("path user ->", os.path.expanduser('~'))
+print("get uid ->", os.environ.get('UID'))
+print("get user ->", os.environ.get('USER'))
+print("get logname ->", os.environ.get('LOGNAME'))
+print("get username ->", os.environ.get(' USERNAME'))
+# print(os.environ.items())
+
+# Print the real user ID of the current process 
+print("get uID ->", os.getuid()) 
+print("get pw id ->",pwd.getpwuid(os.getuid())[0])
+# print(pwd.getpwuid(os.getuid()))
