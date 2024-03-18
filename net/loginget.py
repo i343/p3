fc21f34593ed0c5cbf7b63c25afe1bbb44d30572
R <ivanovich343@gmail.com>
2024-03-18 16:04:17 +0200

    change os login

--- net/loginget.py
+++ net/loginget.py
@@ -1,25 +1,30 @@
 import os
-import pwd
+# import pwd
 
 print("start!")
-osLoginGet = os.getlogin()
 
-print("os get login ->", osLoginGet)
+osName = os.name
+nameLogin = ""
 
-nameLogin = osLoginGet
+match osName:
+    case "nt":
+        print("Windows")
+        nameLogin = os.getlogin()
+    case "posix":
+        print("Unix/MacOS")
+        nameLogin = os.environ.get('USER')
+    case _:
+        print("ОС - невизначено.")
+
+print("os get login ->", nameLogin)
+
 inputLoginStr = 'Input login ['+nameLogin+'] '
 inputLoginGet = input(inputLoginStr)
 if inputLoginGet != "": nameLogin = inputLoginGet
 print("login -> ",nameLogin)
 
-print("path user ->", os.path.expanduser('~'))
-print("get uid ->", os.environ.get('UID'))
-print("get user ->", os.environ.get('USER'))
-print("get logname ->", os.environ.get('LOGNAME'))
-print("get username ->", os.environ.get(' USERNAME'))
+# print("path user ->", os.path.expanduser('~'))
+# print("get uid ->", os.environ.get('UID'))
+# print("get logname ->", os.environ.get('LOGNAME'))
+# print("get username ->", os.environ.get(' USERNAME'))
 # print(os.environ.items())
-
-# Print the real user ID of the current process 
-print("get uID ->", os.getuid()) 
-print("get pw id ->",pwd.getpwuid(os.getuid())[0])
-# print(pwd.getpwuid(os.getuid()))
